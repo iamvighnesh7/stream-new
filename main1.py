df5e7222aea58fdbc200dba4a7f6dac238727a5b
Vighnesh Prabhu <vighneshprabhu554@gmail.com>
2024-11-11 17:47:48 +0530

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from sklearn.datasets import load_iris
+
+st.write('hello world')
+st.write('gm')
+st.balloons()
+st.write('hii')
+st.write('Vighnesh Prabhu')
+
+data = load_iris(as_frame=True).frame
